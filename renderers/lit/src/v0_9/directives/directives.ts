49df893b45749e98b0b986164cd692ef4a2043fa
David Iglesias <ditman@gmail.com>
2026-03-31 10:38:52 -0700

    Add Markdown support to basic Text widget (Lit). (#998)
    * Introduces a `Context` import to use for dependency injection of framework components (in this case: `Context.markdown`).
    * Adds an internal `markdown` directive (the same as in v0.8)
    * Modify the **basic** `Text` component to use `Context.markdown` with the `markdown` directive.
    * Modify `demo:gallery09` to provide a markdown renderer.

--- renderers/lit/src/v0_9/directives/directives.ts
+++ renderers/lit/src/v0_9/directives/directives.ts
@@ -14,10 +14,4 @@
  * limitations under the License.
  */
 
-export type { LitComponentApi } from "./types.js";
-export { A2uiController } from "./a2ui-controller.js";
-export { A2uiSurface } from "./surface/a2ui-surface.js";
-export { A2uiLitElement } from "./a2ui-lit-element.js";
-export { Context } from "./context/context.js";
-export { minimalCatalog } from "./catalogs/minimal/index.js";
-export { basicCatalog } from "./catalogs/basic/index.js";
+export { markdown } from "./markdown.js";
